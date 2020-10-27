b1ba1304e464ab84e0b5da26de8149599519edf3
kamalc995 <kamalsekarit@gmail.com>
2020-10-27 19:44:07 +0530

    first commit

---
+++ src/main/java/com/example/demo/RestControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class RestControllerTest {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/demo/v1/status")
+    @ResponseBody
+    Mono<String> status(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+        return Mono.just("helo");
+    }
+}
